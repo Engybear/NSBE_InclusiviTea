54f9b4a1dbc877e93610a894ba8684e9f0cc6476
Engybeer <na.mutlak@gmail.com>
2024-02-24 14:10:29 -0500

    Cleaned up image search code

--- imageSearch.py
+++ imageSearch.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup as bs
-import wget
+
+
+# query = name + street address + Location
 
 query = "University of Toronto Bookstore College Street Location"
 params = {
@@ -13,9 +15,6 @@ soup = bs(html.content,features="html.parser")
 
 images = soup.select('div img')
 images_url = images[0]['src']
-# print(images_url)
 
-outFileName = "outputImage.png"
+# append images url to end of row
 
-# wget.download(images_url,outFileName)
-check = wget.download(images_url,outFileName)
